52d01715a668b99c3c3b3db55c9e46097cae3d9e
Spenser Black <spenserblack01@gmail.com>
2025-12-28 00:12:34 -0500

    Break up SVG methods

--- lib/github_polyglot/svg.rb
+++ lib/github_polyglot/svg.rb
@@ -6,54 +6,90 @@ require 'nokogiri'
 class GithubPolyglot
   # Generates SVG for language stats
   class SVG
+    attr_reader :width, :height, :radii
+
     # @param [Hash] languages Maps language name symbols to amounts.
-    def initialize(languages)
+    # @param [Integer, Float] width The width of the language bar
+    # @param [Integer, Float] height The height of the language bar
+    # @param [Integer, Float] radii The radii of language bar ends
+    def initialize(languages, width: 300, height: 8, radii: 5)
       @languages = languages
+      @width = width
+      @height = height
+      @radii = radii
     end
 
     # Creates an SVG string
-    def generate(width: 300, height: 8)
-      total_amount = @languages.values.sum.to_f
+    def generate
       mask_id = 'mask'
-      radius = 5
-      view_box = [0, 0, width, height].map(&:to_s).join(' ')
       builder = Nokogiri::XML::Builder.new do |xml|
         xml.svg(xmlns: 'http://www.w3.org/2000/svg', width: width, height: height, viewBox: view_box) do
-          xml.defs do
-            mask(xml, mask_id, width, height, radius)
-          end
-
-          xml.g(mask: "url(##{mask_id})") do
-            language_x = 0.0
-
-            sorted_languages.each do |language, amount|
-              ratio = amount / total_amount
-              language_width = width * ratio
-              fill = color(language)
-              next unless fill
-
-              xml.rect(x: language_x, y: 0, width: language_width, height: height, fill: fill)
-              language_x += language_width
-            end
-          end
+          body(xml, mask_id)
         end
       end
 
       builder.to_xml
     end
 
+    # Alias for radii
+    def radius
+      radii
+    end
+
     private
 
+    # The view box for the SVG
+    def view_box
+      [0, 0, width, height].map(&:to_s).join(' ')
+    end
+
+    # Builds the xml body
+    def body(xml, mask_id)
+      xml.defs do
+        mask(xml, mask_id)
+      end
+      language_group(xml, mask_id)
+    end
+
     # Builds the mask for the rounded corners.
-    def mask(xml, id, width, height, radius)
+    def mask(xml, id)
       xml.mask(id: id) do
         xml.rect(x: 0, y: 0, width: width, height: height, rx: radius, ry: radius, fill: 'white')
       end
     end
 
+    # Builds the language bar
+    def language_group(xml, mask_id)
+      xml.g(mask: "url(##{mask_id})") do
+        language_x = 0.0
+
+        sorted_languages.each do |language, amount|
+          language_x = language_entry(xml, language_x, language, amount)
+        end
+      end
+    end
+
+    # Builds a single language entry for the language bar
+    # @return [Integer, Float] The new offset on the X-axis for the next language entry
+    def language_entry(xml, x_offset, language, amount)
+      ratio = amount / total_amount
+      language_width = width * ratio
+      fill = color(language)
+      return x_offset unless fill
+
+      xml.rect(x: x_offset, y: 0, width: language_width, height: height, fill: fill)
+
+      x_offset + language_width
+    end
+
     # Returns the languages sorted by amount.
     def sorted_languages
       @sorted_languages ||= @languages.sort_by { |_, amount| -amount }
+    end
+
+    # Gets the total amount for languages.
+    def total_amount
+      @total_amount ||= @languages.values.sum.to_f
     end
 
     # Gets the color as a hex code string for a language.
