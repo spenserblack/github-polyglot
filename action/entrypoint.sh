677722197232c0b5b09a4b5e868ef2044026a2c3
Spenser Black <spenserblack01@gmail.com>
2026-06-10 14:04:08 +0000

    Fix output for multiline strings

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 OUTPUT="$(github-polyglot "$@")"
+
 # NOTE: We still want to show the output to the user, which can be helpful if the user is using
 #       the `print` format and just wants to see the output in the action log.
 echo "$OUTPUT"
+
 # NOTE: Save the output to the `output` output of the action
-echo -n "output=$OUTPUT" >> $GITHUB_OUTPUT
+echo 'output<<EOF' >> "$GITHUB_OUTPUT"
+echo "$OUTPUT" >> "$GITHUB_OUTPUT"
+echo "EOF" >> "$GITHUB_OUTPUT"
